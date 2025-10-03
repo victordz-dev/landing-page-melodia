961c6dd1004d8af77e3679cfbf46ca272128e5ea
Victor Rodriguez <164812973+victordz-dev@users.noreply.github.com>
2025-10-03 12:59:21 -0300

    fix: alternando o link do gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 
     root: 'src',
-    base: '/nome-do-seu-repositorio/',
+    base: '/landing-page-melodia/',
 
     build: {
         outDir: '../dist'
